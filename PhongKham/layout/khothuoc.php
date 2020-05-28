d0e04b3a530d1f1ef00af730c0615d34489ee846
thuan0201 <65532551+thuan0201@users.noreply.github.com>
2020-05-28 11:02:35 +0700

    design kho thuoc

---
+++ PhongKham/layout/khothuoc.php
@@ -0,0 +1,69 @@
+<?php 
+	require_once("header.php");
+
+	session_start();
+	include('../config/opp.php');
+	$db = new Database;
+	$db->connect();
+	$count = 6;
+	$page = isset($_GET['page']) ? $_GET['page'] : 1;
+	 $data = $db->xemkhothuoc($page, $count);
+
+	 $tongsoluong = $db->demtongthuoc();
+	 
+
+
+?>
+
+<h5 class="text-center text-uppercase text-danger py-3">quản lý kho thuốc</h5>
+
+<div class="col-8 mx-auto">
+  <a href="themthuoc.php" title="" ><button type="" class="btn btn-info float-right ">THÊM THUỐC</button></a>
+	<table class="table table-hover">
+    <thead>
+      <tr>
+        <th>id</th>
+        <th>name</th>
+        <th>số lượng</th>
+        <th>Đơn vị tính</th>
+        <th>Tác dụng</th>
+        <th>Giá nhập</th>
+        <th>Giá bán</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      	if($data!=0){
+      		foreach ($data as  $value) {
+      			echo '<tr>';
+      			echo '<td>'.$value['id'].'</td>';
+      			echo '<td>'.$value['name'].'</td>';
+      			echo '<td>'.$value['soluong'].'</td>';
+      			echo '<td>'.$value['donvitinh'].'</td>';
+      			echo '<td>'.$value['tacdung'].'</td>';
+      			echo '<td>'.$value['gianhap'].'</td>';
+      			echo '<td>'.$value['giaban'].'</td>';
+      			echo '<td><a href="suathuoc.php?id='.$value['id'].'"><i class="fas fa-edit"></i></a></td>';
+      			echo '</tr>';
+      		}
+      	}
+      ?>
+    </tbody>
+  </table>
+ <?php
+ 	$sotrang = CEIL($tongsoluong / $count);
+ 	 if($sotrang>1){
+ 	 	for($i=1;$i<=$sotrang;$i++){
+ 	 		if($i==$page){
+ 	 			echo '<a href="?page='.$i.'" class="btn border ml-1 bg-success text-light">'.$i.'</a>';
+ 	 		}else
+ 	 		{
+ 	 			echo '<a href="?page='.$i.'" class="btn border ml-1 bg-light">'.$i.'</a>';
+ 	 		}
+ 	 	}
+ 	 }
+ ?>
+</div>
+
+</div>
